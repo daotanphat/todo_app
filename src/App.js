a0a033b0c60a8a0187d273f6e080fa731b9d3c2c
Dao Tan Phat <daotanphat140703@gmail.com>
2024-06-03 23:32:32 +0700

    feat: init react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import { useStore, actions } from './store';
 import './App.css';
+import './store/ToDo.css'
+
 
 function App() {
+  const [state, dispatch] = useStore();
+  const { todos, todoInput } = state;
+
+  const handleAdd = () => {
+    dispatch(actions.addTodo(todoInput));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      {/* <input
+        value={todoInput}
+        placeholder='Enter todo'
+        onChange={e => {
+          dispatch(actions.setTodoInput(e.target.value));
+        }}
+      />
+      <button onClick={handleAdd}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul> */}
+
+      <div class="row">
+        <div class="col-md-12">
+
+          <div class="main-todo-input-wrap">
+            <div class="main-todo-input fl-wrap">
+              <div class="main-todo-input-item">
+                <input type="text" id="todo-list-item"
+                  value={todoInput}
+                  placeholder="What will you do today?"
+                  onChange={e => {
+                    dispatch(actions.setTodoInput(e.target.value))
+                  }}
+                />
+              </div>
+              <button onClick={handleAdd} class="add-items main-search-button">ADD</button>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+      <div className="row">
+        <div className="col-md-12">
+
+          <div className="main-todo-input-wrap">
+            <div className="main-todo-input fl-wrap todo-listing">
+              <ul id="list-items">
+                {todos.map((todo, index) => (
+                  <li key={index}>
+                    <input className="checkbox" type="checkbox" />
+                    <span className="todo-text">{todo}</span>
+                    <a className="remove text-right"><i className="fa fa-trash" onClick={() => alert('Hello')}></i></a>
+                    <hr />
+                  </li>
+                ))}
+              </ul>
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 }
